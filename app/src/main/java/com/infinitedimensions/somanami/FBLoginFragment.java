d9e3977ad33ad1ff51b9d4a8ee38a72d109b44a3
nickhargreaves <hargreaves1234@gmail.com>
2014-12-21 23:31:44 +0300

    take to main screen on login

--- app/src/main/java/com/infinitedimensions/somanami/FBLoginFragment.java
+++ app/src/main/java/com/infinitedimensions/somanami/FBLoginFragment.java
@@ -88,6 +88,9 @@ public class FBLoginFragment extends Fragment {
     private void onSessionStateChange(Session session, SessionState state, Exception exception) {
         if (state.isOpened()) {
             Log.i(TAG, "Logged in...");
+            Intent i = new Intent(getActivity(), MainActivity.class);
+            i.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+            getActivity().startActivity(i);
         } else if (state.isClosed()) {
             Log.i(TAG, "Logged out...");
         }
