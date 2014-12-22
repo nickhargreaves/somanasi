d06418b91816eb636ccdf00cf84f6f3e04b6fc09
nickhargreaves <hargreaves1234@gmail.com>
2014-12-22 15:52:11 +0300

    add book dialog

--- app/src/main/java/com/infinitedimensions/somanami/MainActivity.java
+++ app/src/main/java/com/infinitedimensions/somanami/MainActivity.java
@@ -1,5 +1,6 @@
 package com.infinitedimensions.somanami;
 
+import android.app.Dialog;
 import android.content.Context;
 import android.os.Bundle;
 import android.support.v4.app.FragmentManager;
@@ -96,10 +97,21 @@ public class MainActivity extends ActionBarActivity
         // automatically handle clicks on the Home/Up button, so long
         // as you specify a parent activity in AndroidManifest.xml.
         int id = item.getItemId();
-        if (id == R.id.action_settings) {
+        if (id == R.id.action_add) {
+
+            addDialog();
+
             return true;
         }
         return super.onOptionsItemSelected(item);
+    }
+
+    public void addDialog(){
+        Dialog dialog = new Dialog(MainActivity.this);
+        dialog.setTitle("Add your book");
+        dialog.setContentView(R.layout.dialog_add);
+
+        dialog.show();
     }
 
     @Override
