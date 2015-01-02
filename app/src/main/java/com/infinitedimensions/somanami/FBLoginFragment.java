2526cb13023945a5f4b98e4d1d7af185b1f412a2
nickhargreaves <hargreaves1234@gmail.com>
2015-01-02 22:04:30 +0300

    configs and deploy to appengine

--- app/src/main/java/com/infinitedimensions/somanami/FBLoginFragment.java
+++ app/src/main/java/com/infinitedimensions/somanami/FBLoginFragment.java
@@ -1,5 +1,6 @@
 package com.infinitedimensions.somanami;
 
+import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.AsyncTask;
@@ -10,6 +11,7 @@ import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Toast;
 
 import com.facebook.Request;
 import com.facebook.Response;
@@ -18,6 +20,12 @@ import com.facebook.SessionState;
 import com.facebook.UiLifecycleHelper;
 import com.facebook.model.GraphUser;
 import com.facebook.widget.LoginButton;
+import com.google.android.gms.gcm.GoogleCloudMessaging;
+import com.google.api.client.extensions.android.http.AndroidHttp;
+import com.google.api.client.extensions.android.json.AndroidJsonFactory;
+import com.google.api.client.googleapis.services.AbstractGoogleClientRequest;
+import com.google.api.client.googleapis.services.GoogleClientRequestInitializer;
+import com.infinitedimensions.somanami.somanamigcm.registration.Registration;
 
 import org.apache.http.HttpResponse;
 import org.apache.http.client.methods.HttpGet;
@@ -26,8 +34,11 @@ import org.codehaus.jackson.JsonFactory;
 import org.codehaus.jackson.JsonParser;
 import org.codehaus.jackson.JsonToken;
 
+import java.io.IOException;
 import java.io.InputStream;
 import java.util.Arrays;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 public class FBLoginFragment extends Fragment {
 
@@ -113,6 +124,8 @@ public class FBLoginFragment extends Fragment {
         if (state.isOpened()) {
             Log.i(TAG, "Logged in...");
 
+            new GcmRegistrationAsyncTask(getActivity()).execute();
+
             Request.newMeRequest(session, new Request.GraphUserCallback() {
 
                 // callback after Graph API response with user object
@@ -124,6 +137,8 @@ public class FBLoginFragment extends Fragment {
                         editor.putString("lastname", user.getLastName());
                         editor.putString("email", user.asMap().get("email").toString());
                         editor.commit();
+
+
 
                         url = Defaults.API_URL + "public/register/" + user.getId() + "/" + user.getFirstName() + "/" + user.getLastName() + "/" + user.asMap().get("email");
 
@@ -198,6 +213,68 @@ public class FBLoginFragment extends Fragment {
         protected void onPostExecute(String file_url) {
 
 
+        }
+    }
+
+    class GcmRegistrationAsyncTask extends AsyncTask<Context, Void, String> {
+
+        private Registration regService = null;
+        private GoogleCloudMessaging gcm;
+        private Context context;
+
+        // TODO: change to your own sender ID to Google Developers Console project number, as per instructions above
+        private final String SENDER_ID = getResources().getString(R.string.gcm_project_id);
+
+        public GcmRegistrationAsyncTask(Context context) {
+            this.context = context;
+        }
+
+        @Override
+        protected String doInBackground(Context... params) {
+            if (regService == null) {
+                Registration.Builder builder = new Registration.Builder(AndroidHttp.newCompatibleTransport(),
+                        new AndroidJsonFactory(), null)
+                        // Need setRootUrl and setGoogleClientRequestInitializer only for local testing,
+                        // otherwise they can be skipped
+
+                        .setRootUrl("http://10.0.2.2:8080/_ah/api/")
+                        .setGoogleClientRequestInitializer(new GoogleClientRequestInitializer() {
+                            @Override
+                            public void initialize(AbstractGoogleClientRequest<?> abstractGoogleClientRequest)
+                                    throws IOException {
+                                abstractGoogleClientRequest.setDisableGZipContent(true);
+                            }
+                        });
+                // end of optional local run code
+
+                regService = builder.build();
+            }
+
+            String msg = "";
+            try {
+                if (gcm == null) {
+                    gcm = GoogleCloudMessaging.getInstance(context);
+                }
+                String regId = gcm.register(SENDER_ID);
+                msg = "Device registered, registration ID=" + regId;
+
+                // You should send the registration ID to your server over HTTP,
+                // so it can use GCM/HTTP or CCS to send messages to your app.
+                // The request to your server should be authenticated if your app
+                // is using accounts.
+                regService.register(regId).execute();
+
+            } catch (IOException ex) {
+                ex.printStackTrace();
+                msg = "Error: " + ex.getMessage();
+            }
+            return msg;
+        }
+
+        @Override
+        protected void onPostExecute(String msg) {
+            Toast.makeText(context, msg, Toast.LENGTH_LONG).show();
+            Logger.getLogger("REGISTRATION").log(Level.INFO, msg);
         }
     }
 }
