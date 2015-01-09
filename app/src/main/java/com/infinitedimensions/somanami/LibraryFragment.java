208a8c8f6ab0dc6de95955e2cc5d4b7fceffa2a7
nickhargreaves <hargreaves1234@gmail.com>
2015-01-09 19:10:18 +0300

    remove rendudant code on lib fragmnent

--- app/src/main/java/com/infinitedimensions/somanami/LibraryFragment.java
+++ app/src/main/java/com/infinitedimensions/somanami/LibraryFragment.java
@@ -1,29 +1,21 @@
 package com.infinitedimensions.somanami;
 
 import android.app.Activity;
-import android.app.AlertDialog;
-import android.app.Dialog;
 import android.content.Context;
-import android.content.DialogInterface;
-import android.content.Intent;
 import android.content.SharedPreferences;
-import android.os.AsyncTask;
 import android.os.Bundle;
 import android.preference.PreferenceManager;
-import android.provider.Settings;
 import android.support.v4.app.DialogFragment;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.ActionBar;
 import android.util.DisplayMetrics;
-import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
-import android.widget.RelativeLayout;
 import android.widget.TextView;
 import android.widget.Toast;
 import android.widget.ToggleButton;
@@ -33,15 +25,6 @@ import com.infinitedimensions.somanami.network.RequestBook;
 import com.infinitedimensions.somanami.network.SimpleDBHandler;
 import com.squareup.picasso.Picasso;
 
-import org.apache.http.HttpResponse;
-import org.apache.http.client.methods.HttpGet;
-import org.apache.http.impl.client.DefaultHttpClient;
-import org.codehaus.jackson.JsonFactory;
-import org.codehaus.jackson.JsonParser;
-import org.codehaus.jackson.JsonToken;
-
-import java.io.IOException;
-import java.io.InputStream;
 import java.net.URL;
 import java.net.URLConnection;
 import java.util.ArrayList;
@@ -63,33 +46,12 @@ public class LibraryFragment extends Fragment {
     CardGridArrayAdapter mCardArrayAdapter;
     private int stackSize = 0;
 
-    private ImageView loading_gif;
-    private GifAnimationDrawable little;
-
-    private RelativeLayout rlloading;
-
-    private GPSTracker gpsT;
-    private double latitude;
-    private double longitude;
-    private String location="(0, 0)";
-
-
-    private ConnectionDetector cd;
-    private Boolean isInternetPresent;
-
-    private TextView notification;
-
-    private String last_item = "0";
     private ArrayList<Card> cards;
-    SharedPreferences prefs;
-
-    SharedPreferences.Editor editor;
     CardGridView gridView;
-
-    private int mStackLevel = 0;
     SimpleDBHandler dbHandler;
     private SharedPreferences pref;
     private String user_id;
+    private int mStackLevel = 0;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
@@ -101,30 +63,9 @@ public class LibraryFragment extends Fragment {
 
         rootView = inflater.inflate(R.layout.fragment_library, container, false);
         gridView = (CardGridView) rootView.findViewById(R.id.favoritesGrid);
-        notification = (TextView)rootView.findViewById(R.id.notification);
-
-        loading_gif = (ImageView)rootView.findViewById(R.id.ivLoading);
-
-        rlloading = (RelativeLayout)rootView.findViewById(R.id.rlLoading);
-        rlloading.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                checkConditions();
-            }
-        });
-        // and add the GifAnimationDrawable
-        try{
-
-            little = new GifAnimationDrawable(getResources().openRawResource(R.raw.loading_anim));
-            little.setOneShot(false);
-            loading_gif.setImageDrawable(little);
-        }catch(IOException ioe){
-
-        }
 
         dbHandler = new SimpleDBHandler(getActivity().getApplicationContext(), null, null, 1);
         getBooks();
-        //checkConditions();
         return rootView;
     }
 
@@ -139,88 +80,6 @@ public class LibraryFragment extends Fragment {
 
     }
 
-    public void checkConditions(){
-        notification.setText("Gathering books...");
-
-        gridView.setVisibility(View.GONE);
-        rlloading.setVisibility(View.VISIBLE);
-        //check if has network
-        // creating connection detector class instance
-        cd = new ConnectionDetector(getActivity().getApplicationContext());
-
-        //get Internet status
-        isInternetPresent = cd.isConnectingToInternet();
-
-        if(!isInternetPresent){
-
-            Toast.makeText(getActivity().getApplicationContext(), "Check your internet settings!", Toast.LENGTH_LONG).show();
-            //TODO: show cache
-
-
-            //TODO:timer to check settings
-            notification.setText("Tap to retry!");
-
-        }else{
-            //check location
-            gpsT = new GPSTracker(getActivity().getApplicationContext());
-
-            // check if GPS enabled
-            if(gpsT.canGetLocation()){
-
-                latitude = gpsT.getLatitude();
-                longitude = gpsT.getLongitude();
-
-                location = "("+latitude+", "+longitude+")";
-
-                //new Thread(this).run();
-                new getContent().execute();
-            }else{
-
-                //gpsT.showSettingsAlert();
-
-                FragmentTransaction ft = getFragmentManager().beginTransaction();
-                // Create and show the dialog.
-                allowLocationDialog newFragment = new allowLocationDialog ();
-
-
-                newFragment.show(ft, "dialog");
-
-                //TODO:timer to check settings
-                notification.setText("Tap to retry!");
-
-            }
-        }
-
-
-    }
-
-
-
-    public static class allowLocationDialog extends DialogFragment {
-
-        @Override
-        public Dialog onCreateDialog(Bundle savedInstanceState) {
-            return new AlertDialog.Builder(getActivity())
-                    .setTitle("GPS Settings")
-                    .setMessage("Do you want to go to settings menu? You also need a view of the sky for it work properly.")
-                    .setNegativeButton(android.R.string.no, new DialogInterface.OnClickListener() {
-                        @Override
-                        public void onClick(DialogInterface dialog, int which) {
-                            // cancel
-
-                        }
-                    })
-                    .setPositiveButton(android.R.string.yes,  new DialogInterface.OnClickListener() {
-                        @Override
-                        public void onClick(DialogInterface dialog, int which) {
-                            Intent intent = new Intent(Settings.ACTION_LOCATION_SOURCE_SETTINGS);
-                            intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
-                            getActivity().getApplicationContext().startActivity(intent);
-                        }
-                    })
-                    .create();
-        }
-    }
 
     public static LibraryFragment newInstance(int sectionNumber) {
         LibraryFragment fragment = new LibraryFragment();
@@ -239,75 +98,6 @@ public class LibraryFragment extends Fragment {
                 getArguments().getInt(ARG_SECTION_NUMBER), "");
     }
 
-
-    class getContent extends AsyncTask<String, String, String> {
-        @Override
-        protected void onPreExecute() {
-            super.onPreExecute();
-
-            cards = new ArrayList<Card>();
-            contentList = new ArrayList<Book>();
-
-        }
-        protected String doInBackground(String... args) {
-            DefaultHttpClient httpClient = new DefaultHttpClient();
-            try {
-                String url = Defaults.API_URL + "public/listbooks";
-                HttpResponse response = httpClient
-                        .execute(new HttpGet(url));
-                Log.d("url", "url:" + url);
-
-                InputStream is = response.getEntity().getContent();
-
-                JsonFactory factory = new JsonFactory();
-
-                JsonParser jsonParser = factory.createJsonParser(is);
-
-                JsonToken token = jsonParser.nextToken();
-
-                // Expected JSON is an array so if current token is "[" then while
-                // we don't get
-                // "]" we will keep parsing
-                if (token == JsonToken.START_ARRAY) {
-                    while (token != JsonToken.END_ARRAY) {
-                        // Inside array there are many objects, so it has to start
-                        // with "{" and end with "}"
-                        token = jsonParser.nextToken();
-                        if (token == JsonToken.START_OBJECT) {
-
-                            while (token != JsonToken.END_OBJECT) {
-                                // Each object has a name which we will use to
-                                // identify the type.
-                                token = jsonParser.nextToken();
-                                if (token == JsonToken.FIELD_NAME) {
-                                    String objectName = jsonParser.getCurrentName();
-                                    // jsonParser.nextToken();
-                                    //if (0 == objectName.compareToIgnoreCase("CONTENT")) {
-                                    Book book = new Book();
-                                    book.parse(jsonParser);
-                                    //create card for this content
-                                    contentList.add(book);
-
-                                    //}
-                                }
-                            }
-                        }
-                    }
-                }
-            } catch (Exception e) {
-
-                e.printStackTrace();
-            }
-
-            return null;
-
-        }
-        protected void onPostExecute(String file_url) {
-
-            displayBooks();
-
-        }
-    }
 
     public void displayBooks(){
         stackSize = contentList.size();
@@ -368,8 +158,6 @@ public class LibraryFragment extends Fragment {
             gridView.setAdapter(mCardArrayAdapter);
         }
 
-        rlloading.setVisibility(View.GONE);
-        gridView.setVisibility(View.VISIBLE);
     }
 
     public static class singleBookDialogFragment extends DialogFragment {
