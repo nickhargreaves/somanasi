1dc09f0e9df22e6a52e095fd28d9a171226f0006
nickhargreaves <hargreaves1234@gmail.com>
2015-01-25 12:31:01 +0300

    get content and start alarm service on set up screen

--- app/src/main/java/com/infinitedimensions/somanami/FirstTimeActivity.java
+++ app/src/main/java/com/infinitedimensions/somanami/FirstTimeActivity.java
@@ -5,17 +5,53 @@ import android.app.PendingIntent;
 import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.preference.PreferenceManager;
 import android.support.v7.app.ActionBarActivity;
+import android.util.Log;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
+import android.widget.Toast;
 
+import com.infinitedimensions.somanami.helpers.ConnectionDetector;
+import com.infinitedimensions.somanami.helpers.GifAnimationDrawable;
+import com.infinitedimensions.somanami.helpers.SimpleDBHandler;
+import com.infinitedimensions.somanami.models.Book;
+import com.infinitedimensions.somanami.models.TrayItem;
 import com.infinitedimensions.somanami.network.SyncAlarm;
 
-/**
- * Created by nick on 1/25/15.
- */
-public class FirstTimeActivity extends ActionBarActivity {
+import org.apache.http.HttpResponse;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.impl.client.DefaultHttpClient;
+import org.codehaus.jackson.JsonFactory;
+import org.codehaus.jackson.JsonParser;
+import org.codehaus.jackson.JsonToken;
 
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.ArrayList;
+import java.util.List;
+
+
+public class FirstTimeActivity extends ActionBarActivity {
+    List<Book> contentList;
+    public List<TrayItem> trayList;
+    private ImageView loading_gif;
+    private GifAnimationDrawable little;
+
+    private RelativeLayout rlloading;
+
+    private ConnectionDetector cd;
+    private Boolean isInternetPresent;
+
+    private TextView notification;
+
+    String user_id = "0";
+    SimpleDBHandler dbHandler;
+    SharedPreferences prefs;
     @Override
     protected void onCreate(Bundle savedInstanceState){
         super.onCreate(savedInstanceState);
@@ -24,11 +60,48 @@ public class FirstTimeActivity extends ActionBarActivity {
         //set not first time
         setNotFirstTime();
         //getBooks
-        //getFriends
-        //start alarm service
-        startAlarmService();
-        //redirect to home page
-        
+        notification = (TextView)findViewById(R.id.notification);
+
+        loading_gif = (ImageView)findViewById(R.id.ivLoading);
+        rlloading = (RelativeLayout)findViewById(R.id.rlLoading);
+        rlloading.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                checkConditions();
+            }
+        });
+        // and add the GifAnimationDrawable
+        try{
+
+            little = new GifAnimationDrawable(getResources().openRawResource(R.raw.loading_anim));
+            little.setOneShot(false);
+            loading_gif.setImageDrawable(little);
+        }catch(IOException ioe){
+
+        }
+
+        checkConditions();
+
+    }
+    public void checkConditions(){
+        notification.setText("Setting up...");
+        //check if has network
+        // creating connection detector class instance
+        cd = new ConnectionDetector(getApplicationContext());
+
+        //get Internet status
+        isInternetPresent = cd.isConnectingToInternet();
+
+        if(!isInternetPresent){
+
+            Toast.makeText(getApplicationContext(), "Check your internet settings!", Toast.LENGTH_LONG).show();
+
+            //TODO:timer to check settings
+            notification.setText("Tap to retry!");
+
+        }else{
+                new getContent().execute();
+        }
 
     }
 
@@ -51,4 +124,165 @@ public class FirstTimeActivity extends ActionBarActivity {
         am.cancel(pendingIntent);
         am.set(AlarmManager.RTC, System.currentTimeMillis(), pendingIntent);
     }
+
+    class getContent extends AsyncTask<String, String, String> {
+        @Override
+        protected void onPreExecute() {
+            super.onPreExecute();
+
+            dbHandler = new SimpleDBHandler(getApplicationContext(), null, null, 1);
+
+            prefs = PreferenceManager.getDefaultSharedPreferences(getApplicationContext());
+            user_id = prefs.getString("user_id", "0");
+
+            contentList = new ArrayList<Book>();
+            trayList = new ArrayList<TrayItem>();
+
+        }
+
+        protected String doInBackground(String... args) {
+
+            //getting library
+
+
+            DefaultHttpClient httpClient = new DefaultHttpClient();
+            try {
+                String url = Defaults.API_URL + "public/listbooks";
+                HttpResponse response = httpClient
+                        .execute(new HttpGet(url));
+                Log.d("url", "url:" + url);
+
+                InputStream is = response.getEntity().getContent();
+
+                JsonFactory factory = new JsonFactory();
+
+                JsonParser jsonParser = factory.createJsonParser(is);
+
+                JsonToken token = jsonParser.nextToken();
+
+                // Expected JSON is an array so if current token is "[" then while
+                // we don't get
+                // "]" we will keep parsing
+                if (token == JsonToken.START_ARRAY) {
+                    while (token != JsonToken.END_ARRAY) {
+                        // Inside array there are many objects, so it has to start
+                        // with "{" and end with "}"
+                        token = jsonParser.nextToken();
+                        if (token == JsonToken.START_OBJECT) {
+
+                            while (token != JsonToken.END_OBJECT) {
+                                // Each object has a name which we will use to
+                                // identify the type.
+                                token = jsonParser.nextToken();
+                                if (token == JsonToken.FIELD_NAME) {
+                                    String objectName = jsonParser.getCurrentName();
+                                    // jsonParser.nextToken();
+                                    //if (0 == objectName.compareToIgnoreCase("CONTENT")) {
+                                    Book book = new Book();
+                                    book.parse(jsonParser);
+                                    //create card for this content
+                                    contentList.add(book);
+
+                                    //}
+                                }
+                            }
+                        }
+                    }
+                }
+            } catch (Exception e) {
+
+                e.printStackTrace();
+            }
+
+            //get tray
+            httpClient = new DefaultHttpClient();
+            try {
+                String url = Defaults.API_URL + "public/my_tray/" + user_id;
+                HttpResponse response = httpClient
+                        .execute(new HttpGet(url));
+                Log.d("url", "url:" + url);
+
+                InputStream is = response.getEntity().getContent();
+
+                JsonFactory factory = new JsonFactory();
+
+                JsonParser jsonParser = factory.createJsonParser(is);
+
+                JsonToken token = jsonParser.nextToken();
+
+                // Expected JSON is an array so if current token is "[" then while
+                // we don't get
+                // "]" we will keep parsing
+                if (token == JsonToken.START_ARRAY) {
+                    while (token != JsonToken.END_ARRAY) {
+                        // Inside array there are many objects, so it has to start
+                        // with "{" and end with "}"
+                        token = jsonParser.nextToken();
+                        if (token == JsonToken.START_OBJECT) {
+
+                            while (token != JsonToken.END_OBJECT) {
+                                // Each object has a name which we will use to
+                                // identify the type.
+                                token = jsonParser.nextToken();
+                                if (token == JsonToken.FIELD_NAME) {
+                                    String objectName = jsonParser.getCurrentName();
+                                    // jsonParser.nextToken();
+                                    //if (0 == objectName.compareToIgnoreCase("CONTENT")) {
+                                    TrayItem trayItem = new TrayItem();
+                                    trayItem.parse(jsonParser);
+                                    //create card for this content
+                                    trayList.add(trayItem);
+
+                                    //}
+                                }
+                            }
+                        }
+                    }
+                }
+            } catch (Exception e) {
+
+                e.printStackTrace();
+            }
+
+            return null;
+
+        }
+
+        protected void onPostExecute(String file_url) {
+
+            for(int i = 0; i<contentList.size(); i++) {
+
+                Book b = contentList.get(i);
+
+                if (!dbHandler.bookExists(b.getId())) {
+                    //add book to db
+                    dbHandler.addBook(b);
+                }
+            }
+
+            for(int i = 0; i<trayList.size(); i++) {
+
+                TrayItem t = trayList.get(i);
+
+                if (!dbHandler.trayItemExists(t.getId())) {
+                    //add book to db
+                    dbHandler.addTrayItem(t);
+                }
+
+            }
+
+            //start alarm service for fetching content
+            startAlarmService();
+
+            //redirect to home screen
+            Intent i = new Intent(getApplicationContext(), MainActivity.class);
+            i.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+            i.addFlags(Intent.FLAG_ACTIVITY_NO_HISTORY | Intent.FLAG_ACTIVITY_EXCLUDE_FROM_RECENTS);
+            startActivity(i);
+            finish();
+
+        }
+
+    }
+
 }
